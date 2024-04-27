954c349e6e5630d67b0113edb3c37ab4e85a120f
Juan Marcano <juancrlsmarcano@gmail.com>
2024-04-27 13:51:08 -0500

    inicial

---
+++ funciones.py
@@ -0,0 +1,120 @@
+from PyPDF2 import PdfWriter, PdfReader
+import fitz
+import os
+
+#inputfile_path = "H:\My Drive\RECORTE PDF\ml.pdf"
+#inputfile_path2 = "H:\My Drive\RECORTE PDF\ml_salida2.pdf"
+#outputfile_path = "H:\My Drive\RECORTE PDF\ml_salida5.pdf"
+
+def recortar_etiquetas_ml(inputfile_path,outputfile_path):
+    print("Iniciando recorte de etiquetas...")
+    reader1 = PdfReader(inputfile_path)  
+    reader2 = PdfReader(inputfile_path)
+    reader3 = PdfReader(inputfile_path)  
+    writer = PdfWriter()
+    recorte_inferior=100
+
+    totalPages1 = len(reader1.pages)
+    print(f"Páginas Totales del documento de entrada: {totalPages1}")
+
+    for i in range(totalPages1-1):
+    
+   
+        page1 = reader1.pages[i]
+        
+        #print (page1.mediabox.right)
+        #print (page1.mediabox.top)
+        page1.mediabox.upper_right = (
+            page1.mediabox.right-550,
+            page1.mediabox.top,
+        )
+        page1.mediabox.lower_left = (
+            page1.mediabox.left+25,
+            page1.mediabox.bottom+recorte_inferior,
+        )
+
+ 
+        writer.add_page(page1)
+
+        page2 = reader2.pages[i]
+        #print (page2.mediabox.right)
+        #print (page2.mediabox.top)
+        page2.mediabox.upper_right = (
+            page2.mediabox.right-285,
+            page2.mediabox.top,
+        )
+        page2.mediabox.lower_left = (
+            page2.mediabox.left+290,
+            page2.mediabox.bottom+recorte_inferior,
+        )
+
+        writer.add_page(page2)
+
+        page3 = reader3.pages[i]
+        #print (page3.mediabox.right)-
+        #print (page3.mediabox.top)
+        page3.mediabox.upper_right = (
+            page3.mediabox.right-20,
+            page3.mediabox.top,
+        )
+        page3.mediabox.lower_left = (
+            page3.mediabox.left+555,
+            page3.mediabox.bottom+recorte_inferior,
+        )
+    
+        writer.add_page(page3)
+    with open(outputfile_path, "wb") as fp:
+        writer.write(fp)
+    totalPages1 = len(writer.pages)
+    #print(f"Páginas Totales salida intermedia: {totalPages1}")    
+   
+def contar_etiquetas(inputfile_path):
+    print("Realizando conteo de etiquetas")
+    texto=""
+    input_pdf = fitz.open(inputfile_path)
+    for pgno in range(input_pdf.page_count-1):
+        page = input_pdf[pgno]
+        texto = texto + page.get_text()
+    cuenta_flex=texto.count("Envío Flex")
+    cuenta_mercado_envios=texto.count("Ciudad de destino:")
+    cuenta=cuenta_flex+cuenta_mercado_envios
+    print("Se encontraron " + str(cuenta_flex)+" etiquetas FLEX en el archivo.")
+    print("Se encontraron " + str(cuenta_mercado_envios)+" etiquetas mercado envíos en el archivo.")
+    print("Se encontraron en total:" + str(cuenta)+" etiquetas.")
+    input_pdf.close()
+    return cuenta
+
+def extraer_etiquetas(inputfile_path,outputfile_path,count):
+    print("Realizando extraccion de "+str(count)+" etiquetas")
+    input_pdf = fitz.open(inputfile_path)
+    output_pdf = fitz.open()
+    for pgno in range(count):
+        #page = input_pdf[pgno]
+        output_pdf.insert_pdf(input_pdf,from_page=pgno,to_page = pgno)
+    input_pdf.close()
+    output_pdf.save(outputfile_path)
+    
+    output_pdf.close()
+
+def contar_recortar_extraer(filename):
+    entrada=filename
+    salida=filename
+    salida=salida.replace(".pdf","_RECORTADO.pdf")
+    cantidad=contar_etiquetas(entrada)
+    recortar_etiquetas_ml(entrada,salida)
+    extraer_etiquetas(salida,salida,cantidad)
+    mensaje="Se guardará una copia recortada del archivo:\n\n"+ salida+"\n\n Cantidad de etiquetas: " + str(cantidad)+"\n\n"
+    print(salida)
+    os.system("start " + salida)
+    #showinfo(
+     #   title='Archivo Seleccionado',
+     #   message=mensaje
+    #)
+    
+    
+
+
+#recortar_etiquetas_ml(inputfile_path,outputfile_path)
+#extraer_etiquetas(outputfile_path,outputfile_path,contar_etiquetas(inputfile_path))
+
+
---
+++ main.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import sys
+import funciones
+import os
+
+
+try:
+    filename=sys.argv[1]
+    #if "open with" has been used
+    print(filename)
+    funciones.contar_recortar_extraer(filename)
+    
+except:
+    #do nothing
+    pass
+
+
+
+ #create the root window
+root = tk.Tk()
+root.title('Recortador de Etiquetas 1.2             Enero 2024-Juan Marcano')
+root.resizable(False, False)
+root.geometry('500x100')
+
+
+
+def select_file():
+    filetypes = (
+        ('archivos pdf', '*.pdf'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Abrir un Archivo',
+        initialdir='/',
+        filetypes=filetypes)
+    
+    funciones.contar_recortar_extraer(filename)
+    
+    
+    showinfo(
+        title='Archivo Seleccionado',
+        message="Se ha guardado una copia recortada del documento"
+    )
+   
+# open button
+
+open_button = ttk.Button(
+    root,
+    text='Abrir un archivo',
+    command=select_file,
+)
+
+open_button.pack(expand=True)
+
+# run the application
+root.mainloop()
